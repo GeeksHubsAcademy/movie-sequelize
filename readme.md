1da55c7e3bb9062fa9e7974b2db979f30b2d8fec
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-10-06 12:06:38 +0200

    movie seeder done

---
+++ readme.md
@@ -0,0 +1,26 @@
+<p align="center">
+    <img src="https://github.com/GeeksHubsAcademy/2020-geekshubs-media/blob/master/image/logo.png" >	
+</p>
+
+# Quick start
+
+## Run the backend
+
+- npm install
+
+- npx nodemon index.js
+
+## DB
+
+sequelize db:create
+sequelize db:migrate
+
+sequelize db:seed:all
+
+importante: cambiar la configuración de MySQL para permitir mayor max_allowed_packet en my.ini
+en windows es: C:\xampp\mysql\bin\my.ini 
+
+sino desde el terminal de MySQL
+mysql -u root
+seteamos:
+set global max_allowed_packet=1000000000;
--- seeders/20201006082601-MovieSeeder.js
+++ seeders/20201006082601-MovieSeeder.js
@@ -1,19 +1,39 @@
 'use strict';
+const axios = require('axios');
+
+const addMovies = async(movies, page) => {
+    const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&append_to_response=credits&language=es-ES&page=' + page);
+    const now = new Date();
+    const moviesChunk = res.data.results.map(movie => ({
+        title: movie.title,
+        poster_path: movie.poster_path,
+        release_date: movie.release_date,
+        vote_average: movie.vote_average,
+        overview: movie.overview,
+        createdAt: now,
+        updatedAt: now
+    }))
+    movies.push(...moviesChunk);
+    return res.data.total_pages
+}
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        const movies = [{
-            release_date: "2012-04-25",
-            id: 24428,
-            vote_average: 7.7,
-            title: "Los Vengadores",
-            overview: "Cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial, Nick Fury, director de la Agencia SHIELD, decide reclutar a un equipo para salvar al mundo de un desastre casi seguro. Adaptación del cómic de Marvel \"Los Vengadores\", el legendario grupo de superhéroes formado por Ironman, Hulk, Thor y el Capitán América entre otros.",
-            poster_path: "\/tyKKKSvjEgDVQ6vtm8vzL5Zx06c.jpg",
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }];
+        try {
 
-        await queryInterface.bulkInsert('Movies', movies, {});
+            const movies = [];
+            const total_pages = await addMovies(movies, 1)
+                //muta la array
+                // movies = movies.concat(first20Movies)//concatena la array
+            for (let i = 2; i < total_pages; i++) {
+                await addMovies(movies, i);
+                console.log(i)
+            }
+            console.log(movies.length);
+            await queryInterface.bulkInsert('Movies', movies, {});
+        } catch (error) {
+            console.error(error.message)
+        }
     },
 
     down: async(queryInterface, Sequelize) => {
